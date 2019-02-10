1c287da7b4a9860bdb84229c23bd106befcba8ae
Mayank Pawar <mayank.pawar@care.com>
2019-02-10 20:16:09 +0530

    adding collectionview class

---
+++ ZoloAssignment/Views/HeaderCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  ZoloAssignment
+//
+//  Created by Mayank on 10/02/19.
+//  Copyright © 2019 Mayank. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ ZoloAssignment/Views/PrimaryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PrimaryCollectionViewCell.swift
+//  ZoloAssignment
+//
+//  Created by Mayank on 10/02/19.
+//  Copyright © 2019 Mayank. All rights reserved.
+//
+
+import UIKit
+
+class PrimaryCollectionViewCell: UICollectionViewCell {
+    
+}
